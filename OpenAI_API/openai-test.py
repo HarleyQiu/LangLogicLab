383c7ae878fc475eee47206609b35b1d4be6f533
QiuLinhao <210554114@qq.com>
2023-12-21 18:44:43 +0800

    一些没什么用的代码

--- OpenAI_API/openai-test.py
+++ OpenAI_API/openai-test.py
@@ -4,8 +4,8 @@ client = OpenAI()
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "你是一个富有诗意的助手，擅长用创造性的天赋解释复杂的编程概念。"},
-    {"role": "user", "content": "写一首诗来解释编程中递归的概念。"}
+    {"role": "system", "content": "你是助人为乐的AI"},
+    {"role": "user", "content": "你会干什么"}
   ]
 )
 
